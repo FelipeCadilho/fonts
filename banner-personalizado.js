6173b11f67ee51e3dfd98edd269eb6bd0ab634fb
Felipe Cadilho <93165162+FelipeCadilho@users.noreply.github.com>
2024-09-22 16:09:30 -0300

    Create banner-personalizado.js

---
+++ banner-personalizado.js
@@ -0,0 +1,20 @@
+<script type="text/javascript">
+alert('Olá, mundo');
+  let count =1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+    nextImage();
+}, 2000)
+
+
+function nextImage(){
+    count++;
+    if(count>4){
+        count = 1;
+    }
+
+    document.getElementById("radio"+count).checked = true;
+}
+
+</script>
